a422cdf7f7de34a563989dd9f78e4773ce3c4a63
joankirui <joankirui99@gmail.com>
2023-08-28 12:10:11 +0300

    add final code for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    screens: {
+      'sm':'640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
